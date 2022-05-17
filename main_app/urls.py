0cb8f6bdf0b65341197eac92bcda29f962990298
Zoneam <zoneam@gmail.com>
2022-05-17 15:16:34 -0700

    adding delete and update views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
 from os import abort
 from django.urls import path
+
 # from the current dir .
 # import the view file
 from . import views
+from django.urls import reverse
 
 urlpatterns = [
     # http://localhost:8000
@@ -13,8 +15,12 @@ urlpatterns = [
 
     # http://localhost:8000/gorillas/
     path('gorillas/', views.gorillas_index, name='index'),
-    
+
     # http://localhost:8000/gorillas/:gorillas_id
     # _id is integer
-    path('gorillas/<int:gorilla_id>', views.gorilla_detail, name='detail')
+    path('gorillas/<int:gorilla_id>', views.gorilla_detail, name='detail'),
+
+    path('gorillas/create/', views.GorillaCreate.as_view(), name='gorillas_create'),
+    path('gorillas/<int:pk>/update/', views.GorillaUpdate.as_view(), name='gorillas_update'),
+    path('gorillas/<int:pk>/delete', views.GorillaDelete.as_view(), name='gorillas_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Gorilla
+from django.urls import reverse
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -47,3 +49,21 @@ def gorilla_detail(request, gorilla_id):
         return render(request, 'gorillas/detail.html', { 'gorilla': gorilla })
     except Gorilla.DoesNotExist:
         return HttpResponseNotFound("<h1 style='text-align: center; margin-top: 200px;'>ERROR <span style='color: red; font-size:70px'>404</span> PAGE NOT FOUND !</h1>")
+    
+    
+class GorillaCreate(CreateView):
+    model = Gorilla
+    fields = '__all__'
+    # success_url = '/gorillas/'
+    
+    def get_success_url(self, **kwargs):
+        return reverse('detail', args=(self.object.id,))
+    
+class GorillaUpdate(UpdateView):
+    model = Gorilla
+    fields = ['description', 'age']
+    success_url = '/gorillas/'
+    
+class GorillaDelete(DeleteView):
+    model = Gorilla
+    success_url = '/gorillas/'
